fd3a516d63baae9a637764fb1ae5eb23e2370518
ifootmark <ifootmark@163.com>
2016-06-14 01:58:03 +0800

    create

---
+++ fm.config.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = { 
+    proxy_prefix: "api",
+    proxy_port: 5000,
+    api_hostName: "127.0.0.1", 
+    api_port: 8080
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var nodeproxy=require("./lib/proxy");
+var fmconfig = require('./fm.config.js');
+var proxy_prefix=!!fmconfig.proxy_prefix?fmconfig.proxy_prefix+"/":"",
+    proxy_port=fmconfig.proxy_port;
+
+var proxy = {
+    proxy_start:function(){
+        app.get('/'+proxy_prefix+'*', nodeproxy.proxy);
+        app.post('/'+proxy_prefix+'*', nodeproxy.proxy);
+
+        var server = app.listen(proxy_port, function () {
+            var host = server.address().address;
+            var port = server.address().port;
+            console.log('Proxy listening at http://%s:%s', host, port);
+        });
+    }
+};
+module.exports=proxy;
