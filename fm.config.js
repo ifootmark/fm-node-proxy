8387156fca6aed5788770395036ab761e0f63711
ifootmark <ifootmark@163.com>
2016-07-18 17:08:27 +0800

    update

--- fm.config.js
+++ fm.config.js
@@ -1,7 +1,21 @@
 'use strict';
-module.exports = { 
-    proxy_prefix: "api",
+
+var options = {
     proxy_port: 5000,
-    api_hostName: "127.0.0.1", 
-    api_port: 8080
+    proxy_prefix: "api/",
+    api_hostName: "127.0.0.1",
+    api_port: 8080,
+    api_prefix: 'proxy/'
 };
+
+try{
+    var config = require('../../fm.config.js');
+    for(var key in options){
+        if (config[key] === undefined || config[key] === null) {
+            config[key] = options[key];
+        }
+    }
+    module.exports = config;
+}catch(e){
+    module.exports = options;
+}
--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var nodeproxy=require("./lib/proxy");
-var fmconfig = require('./fm.config.js');
-var proxy_prefix=!!fmconfig.proxy_prefix?fmconfig.proxy_prefix+"/":"",
-    proxy_port=fmconfig.proxy_port;
-
-var proxy = {
+var proxy = require('./lib/app.js');
+var server = require('./lib/server.js');
+var m = {
     start:function(){
-        app.get('/'+proxy_prefix+'*', nodeproxy.proxy);
-        app.post('/'+proxy_prefix+'*', nodeproxy.proxy);
-
-        var server = app.listen(proxy_port, function () {
-            var port = server.address().port;
-            console.log('Reverse Proxy listening at port %s', port);
-        });
+        proxy.start();
+    },
+    server:{
+        start:function(){
+            server.start();
+        }
     }
 };
-module.exports=proxy;
+module.exports = m;
